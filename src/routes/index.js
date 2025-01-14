4d0bb48206517bb49ed30ad3139209fe9ccdc76a
Hoàng Hiếu Linh <144095603+ITHieuLinh@users.noreply.github.com>
2025-01-14 21:46:51 +0700

    Test

---
+++ src/routes/index.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+
+const homeRoutes = require('./homeRoutes');
+
+router.use('/home', homeRoutes);
+
+module.exports = router;
---
+++ src/utils/responseHelper.js
@@ -0,0 +1,5 @@
+exports.createSuccessResponse = (data) => ({
+    status: "success",
+    data,
+  });
+    
